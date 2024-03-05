981f8fce74d6ae36e08296d90e2f4384a6a54d24
YourKael <153153105+YourKael@users.noreply.github.com>
2024-03-05 15:40:34 +0800

    Add files via upload

---
+++ input.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter 1st number: "))
+
+num2 = int(input("Enter 1st number: "))
+
+sum = num1 + num2
+print(f"The sum of {num1} and {num2} is: {sum}")
---
+++ python.py
@@ -0,0 +1,12 @@
+print("Hello World")
+
+x = 25
+message ="Hi"
+ratio = 3.14
+
+print(type(message))
+print(type(ratio))
+print(type(x))
+
+print(message + "My name is Sean. I am" + x + "years old.")
+print(f"{message} my name is Sean. I am {x} years old.")
